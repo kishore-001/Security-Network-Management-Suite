9c1654564759849f867cf208616c4f934d167a48
kishore-001 <kishoreedu07@gmail.com>
2025-06-04 17:45:23 +0530

    added the project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
